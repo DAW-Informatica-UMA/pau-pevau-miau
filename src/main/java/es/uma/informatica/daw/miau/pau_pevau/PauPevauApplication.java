8cd94b2d9ee14b98322811da044953bfa003bff1
Mihai Cristian Mita <mcmita@uma.es>
2026-04-01 17:56:55 +0200

    feat: initialize spring boot project structure
    Initial setup using Spring Initializr with Maven, JPA, H2, and Lombok.

---
+++ src/main/java/es/uma/informatica/daw/miau/pau_pevau/PauPevauApplication.java
@@ -0,0 +1,13 @@
+package es.uma.informatica.daw.miau.pau_pevau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauPevauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PauPevauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uma/informatica/daw/miau/pau_pevau/PauPevauApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.informatica.daw.miau.pau_pevau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauPevauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
