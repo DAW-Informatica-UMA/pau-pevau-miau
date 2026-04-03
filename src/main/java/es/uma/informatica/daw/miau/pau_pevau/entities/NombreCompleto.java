0bcecd84666be74df613add70f70ce86afc3ca76
erdlm222UMA <victormartinez@uma.es>
2026-04-03 14:00:43 +0200

    Creada la entidad NombreCompleto.java

---
+++ src/main/java/es/uma/informatica/daw/miau/pau_pevau/entities/NombreCompleto.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NombreCompleto {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+}
