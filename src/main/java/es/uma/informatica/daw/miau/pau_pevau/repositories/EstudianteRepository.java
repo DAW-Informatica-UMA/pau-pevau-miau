7e1de0c73571772e905cc685af4c715b810cb655
Francisco Bravo Bravo <key13bb@gmail.com>
2026-04-09 20:11:36 +0200

    Started adding ordering methods.

--- src/main/java/es/uma/informatica/daw/miau/pau_pevau/repositories/EstudianteRepository.java
+++ src/main/java/es/uma/informatica/daw/miau/pau_pevau/repositories/EstudianteRepository.java
@@ -1,8 +1,13 @@
 package es.uma.informatica.daw.miau.pau_pevau.repositories;
 
 import es.uma.informatica.daw.miau.pau_pevau.entities.Estudiante;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
-	
+
+    List<Estudiante> findBySedeId(Long idSede);
+
+    List<Estudiante> findBySedeId(Long idSede, Sort sort);
 }
