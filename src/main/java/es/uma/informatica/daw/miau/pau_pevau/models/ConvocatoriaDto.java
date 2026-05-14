aeb46e14df810bbe79cdc44fb13b6a2c74426ff1
key13bb <key13bb@gmail.com>
2026-05-14 17:17:06 +0200

    Handle Convocatoria objects from other microservice
    Signed-off-by: key13bb <key13bb@gmail.com>

---
+++ src/main/java/es/uma/informatica/daw/miau/pau_pevau/models/ConvocatoriaDto.java
@@ -0,0 +1,11 @@
+package es.uma.informatica.daw.miau.pau_pevau.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ConvocatoriaDto {
+    private Long id;
+    private String cursoAcademico;
+}
