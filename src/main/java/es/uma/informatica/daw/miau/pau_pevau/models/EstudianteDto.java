f6a59490d22c4699c09fcf8655a337fff6eafaee
key13bb <key13bb@gmail.com>
2026-05-14 17:18:24 +0200

    Add names for swagger and identificate in DB's schema
    Signed-off-by: key13bb <key13bb@gmail.com>

--- src/main/java/es/uma/informatica/daw/miau/pau_pevau/models/EstudianteDto.java
+++ src/main/java/es/uma/informatica/daw/miau/pau_pevau/models/EstudianteDto.java
@@ -1,11 +1,13 @@
 package es.uma.informatica.daw.miau.pau_pevau.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+@Schema(name = "Estudiante")
 @Data
 @NoArgsConstructor
 public class EstudianteDto {
